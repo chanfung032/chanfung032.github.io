04226855b5d05af7adfb18e7f26ef8e1e6ab9b2f
chanfung032 <chanfung032@gmail.com>
2017-10-19 20:45:53 +0800

    测试

--- _build/html/_sources/index.txt
+++ _build/html/_sources/index.txt
@@ -1,12 +1,5 @@
-.. My Blog documentation master file, created by
-   sphinx-quickstart on Thu Oct 19 19:53:19 2017.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to My Blog's documentation!
+Blog
 ===================================
-
-内容1:
 
 .. toctree::
    :maxdepth: 2
--- _build/html/searchindex.js
+++ _build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{"\u5185\u5bb9":[],"\u5185\u5bb91":0,content:[],index:[],modul:[],page:[],search:[]},titles:["Welcome to My Blog&#8217;s documentation!"],titleterms:{blog:0,document:0,indic:[],tabl:[],welcom:0}})
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{"\u5185\u5bb9":[],"\u5185\u5bb91":[],content:[],index:[],modul:[],page:[],search:[]},titles:["Blog"],titleterms:{blog:0,document:[],indic:[],tabl:[],welcom:[]}})
